3df402d2b8266b52500492a80df42a8d68fa4a73
iaalm <iaalmsimon@gmail.com>
2016-04-28 10:46:40 +0800

    visual the model_id.json file

---
+++ log_visual.py
@@ -0,0 +1,23 @@
+#!python3
+
+import matplotlib.pyplot as plt
+import json
+import sys
+
+data = json.load(open(sys.argv[1]))
+val_name = ["CIDEr","Bleu_1","Bleu_2","Bleu_3","Bleu_4","METEOR","ROUGE_L"]
+val_data = data["val_lang_stats_history"]
+val_data = sorted(val_data.items(),key=lambda t:int(t[0]))
+
+print(val_data[-1][0])
+for name in val_name:
+    x = []
+    y = []
+    for k,v in val_data:
+        x.append(int(k))
+        y.append(float(v[name]))
+    line = plt.plot(x,y,label=name)[0]
+    plt.text(x[-1]+1000,y[-1]-0.01,name,color=line.get_color())
+    print(name,y[-1])
+#plt.legend(loc='upper left')
+plt.show()
