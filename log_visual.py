4aed0434a5d62f7838aed77a1207089bc21476da
iaalm <iaalmsimon@gmail.com>
2016-04-28 17:50:13 +0800

    recurrent log_visual

--- log_visual.py
+++ log_visual.py
@@ -5,20 +5,37 @@ import json
 import sys
 import os
 
-data = json.load(open(os.path.join(sys.argv[1],'model_id.json')))
-val_name = ["CIDEr","Bleu_1","Bleu_2","Bleu_3","Bleu_4","METEOR","ROUGE_L"]
-val_data = data["val_lang_stats_history"]
-val_data = sorted(val_data.items(),key=lambda t:int(t[0]))
+startx = 0
+dirname = sys.argv[1]
+dirs = [dirname]
+if not(len(sys.argv) > 2 and sys.argv[2] == '-n'):
+    while dirname:
+        data = json.load(open(os.path.join(dirname,'model_id.json')))
+        dirname = os.path.dirname(data['opt']['start_from'])
+        if dirname and dirname not in dirs and os.path.exists(os.path.join(dirname,'model_id.json')):
+            dirs.append(dirname)
+        else:
+            break
+    dirs.reverse()
+print(dirs)
 
-print(val_data[-1][0])
+for dirname in dirs:
+    data = json.load(open(os.path.join(dirname,'model_id.json')))
+    val_name = ["CIDEr","Bleu_1","Bleu_2","Bleu_3","Bleu_4","METEOR","ROUGE_L"]
+    val_data = data["val_lang_stats_history"]
+    val_data = sorted(val_data.items(),key=lambda t:int(t[0]))
+
+    for name in val_name:
+        x = []
+        y = []
+        for k,v in val_data:
+            x.append(int(k)+startx)
+            y.append(float(v[name]))
+        plt.plot(x,y,label=name)[0]
+    startx = startx + int(val_data[-1][0])
+print(startx)
 for name in val_name:
-    x = []
-    y = []
-    for k,v in val_data:
-        x.append(int(k))
-        y.append(float(v[name]))
-    line = plt.plot(x,y,label=name)[0]
-    plt.text(x[-1]*1.01,y[-1]-0.01,name,color=line.get_color())
-    print(name,y[-1])
+    plt.text(startx*1.02,val_data[-1][1][name]-0.01,name)
+    print(name,val_data[-1][1][name])
 #plt.legend(loc='upper left')
 plt.show()
